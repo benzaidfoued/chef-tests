8e17cdb123755e95531d474cf05f16e73cf5d2f5
foued007 <benzaid.foued@gmail.com>
2018-06-16 06:02:57 -0400

    adding cookbook-1

---
+++ cookbooks/cookbook-1/README.md
@@ -0,0 +1,4 @@
+# cookbook-1
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/cookbook-1/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cookbook-1
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
